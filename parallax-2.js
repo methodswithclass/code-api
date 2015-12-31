35a50e50682cf0e087dcb6087f73a90e20e9c222
methodswithclass <chris@methodswithclass.com>
2015-12-31 14:33:07 -0500

    add inner

--- parallax-2.js
+++ parallax-2.js
@@ -33,39 +33,19 @@ angular.module("parallaxModule", [])
 	    	var aspect;
 
 			var goodAspect = function (width, height) {
-
-				if (Math.abs(width/height - aspect) < 0.01) {
-					return true;
-				}
-
+				if (Math.abs(width/height - aspect) < 0.01) return true;
 				return false;
 			}
 
 			var checkHeight = function (height) {
-
-		        if (height < space.height()) {
-
-		            return "under";
-		        }
-		        else if (height > space.height()*1.2) {
-
-		            return "over";
-		        }
-
+		        if (height < space.height()) return "under";
+		        else if (height > space.height()*1.2) return "over";
 		        return "good";
-
 		    }
 
 		    var checkWidth = function (width) {
-
-		        if (width < space.width()) {
-		            return "under";
-		        }
-		        else if (width > space.width()*1.5) {
-
-		            return "over";
-		        }
-
+		        if (width < space.width()) return "under";
+		        else if (width > space.width()*1.5) return "over";
 		        return "good";
 		    }
 
@@ -96,17 +76,6 @@ angular.module("parallaxModule", [])
 		                width = height*aspect;
 		            }
 		        }
-		       
-		       	
-		       	if (checkWidth(width) && checkHeight(height)) {
-		       		//console.log("image good");
-		       		if (goodAspect(width, height)) {
-		       			//console.log("aspect is good " + name + " " + aspect);
-		       		}
-		       		else {
-			       		//console.log("aspect is bad " + name + " " + aspect);
-			       	}
-		       	}
 
 		        img.css({height:height, width:width});
 	    	}
@@ -115,85 +84,78 @@ angular.module("parallaxModule", [])
 
 
 		var inner;
-		var $inner;
 		var img;
-		var hasImg;
-
-		if ($scope.src) {
-
-			hasImg = true;
-
-			inner = document.createElement("div");
-			$(inner).addClass("relative height150 width black-back border z-minus-100");
-			$(element).append(inner);
-
-
-			img = document.createElement("img");
-			$(img).addClass("absolute height80 width-auto center");
-			img.src = $scope.src;
-			$(inner).append(img);
-
-		}
-		else {
-			hasImg = false;
-		}
-
 		var total;
 		var top;
 		var offset;
+		var active = false;
+
+		var setup = function () {
+
+			if ($scope.src && !$scope.inner) {
+
+				active = true;
+
+				inner = document.createElement("div");
+				$(inner).addClass("relative height150 width black-back border z-minus-100");
+				$(element).append(inner);
+
+
+				img = document.createElement("img");
+				$(img).addClass("absolute height80 width-auto center");
+				img.src = $scope.src;
+				$(inner).append(img);
+
+				console.log("parallax: image");
+
+			}
+			else if ($scope.inner && !scope.src) {
+
+				active = true;
+
+				inner = $(element).find("#" + $scope.inner)[0];
+				console.log("parallax: no image, inner id is: " + inner.id);
+			}
+		}
 
 		var reset = function () {
-			if (!hasImg) {
-				inner = $(element).find(".inner")[0];
-				console.log("inner id is: " + inner.id);
-			}
 			fix({img:$(img), space:$(element), first:true});
 			total = 0.9*Math.abs($(inner).height() - $(element).height());
 			initial = -$scope.position*total;
 		}
 
 		var scroll = function () {
-			if (device.valid()) {
+			if (device.valid() && active) {
 				offset = $(element).offset().top - el.offset().top;
 
 				if ($scope.top) top = $scope.factor*offset/1200*total + initial;
 				else top = $scope.factor*(1-offset/1200)*total + initial;
 
-				//console.log("top: " + $scope.top + " factor: " + $scope.factor + " offset: " + offset + " total: " + total + " top: " + top);
-
-				console.log("bottom");
-
 				$(inner).css({top:top});
 			}
+
+			console.log("version");
 		}
 
 		setTimeout(function () {
+			setup();
 			reset();
 			scroll();
-			//$(inner).css({top:initial});
-
-			//console.log("done");
-
-			// setInterval(function () {
-
-			// 	scroll();
-
-			// }, 10);
-
 		}, 300);
-
-		el.bind('scroll', scroll);
 
 		angular.element($window).bind('resize', function () {
 			reset();
 			scroll();
 		});
 
+		el.bind('scroll', scroll);
+
 	}
 
 	return {
 		scope:{
 			src:"@",
+			inner:"@",
 			scroll:"@",
 			top:"=",
 			position:"@",
