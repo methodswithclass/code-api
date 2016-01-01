0039ed78ee2fd261d780dbf27a748a400879f1c3
methodswithclass <chris@methodswithclass.com>
2016-01-01 00:17:05 -0500

    update

--- parallax-2.js
+++ parallax-2.js
@@ -147,7 +147,7 @@ angular.module("parallaxModule", [])
 
 				if ($scope.top) top = -o*0.99;
 				//else top = -o/h*total - shift;
-				else top = -o*0.9 - g;
+				else top = -o*0.8;
 
 				$(inner).css({top:top});
 			}
