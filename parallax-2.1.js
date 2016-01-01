5e97fe58cd994b2a3c428d7ea8344450f7d49cae
methodswithclass <chris@methodswithclass.com>
2016-01-01 01:36:37 -0500

    update

--- parallax-2.1.js
+++ parallax-2.1.js
@@ -149,7 +149,7 @@ angular.module("parallaxModule", [])
 				if (!g) g = (ph-ih)/2;
 
 				if (!m) m = (g-ih)/h*0.9;
-				if (!b) b = -1*g*0.9;
+				if (!b) b = -1*g*1.1;
 
 				if ($scope.top) top = -o*0.99;
 				else top = o*m + b;
