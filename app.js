a42f3c30fd6d88d8e3c4092560a67ab352b9fef4
methodswithclass <chris@methodswithclass.com>
2016-01-03 22:56:04 -0500

    update

---
+++ app.js
@@ -0,0 +1,147 @@
+
+var past = "past";
+var current = "current";
+var development = "development";
+
+var app = angular.module("app", ['parallaxModule'])
+
+
+.factory("data", function () {
+
+	var modules = [
+	{
+		name:"parallax",
+		description:"parallax scrolling angular module. Scrolls an image or DOM element at reduced rate compared to document scroll. Takes image source or child element id as input.",
+		versions:[
+		{
+			number:"1",
+			src:"parallax-1.js",
+			status:past
+		},
+		{
+			number:"2",
+			src:"parallax-2.js",
+			status:past
+		},
+		{
+			number:"2.1",
+			src:"parallax-2.1.js",
+			status:current
+		}
+		]
+	},
+	{
+		name:"classes",
+		description:"all purpose css library. allows you to style any element anyway you want without making a special class for it.",
+		versions:[
+		{
+			number:"1",
+			src:"classes.css",
+			status:current
+		}
+		]
+	},
+	{
+		name:"shared",
+		description:"contains several services for any angular project. includes a generic events service and a generic service to send and receive any kind of object or data within an angular app",
+		versions:[
+		{
+			number:"1",
+			src:"shared-1.js",
+			status:past
+		},
+		{
+			number:"2",
+			src:"shared-2.js",
+			status:current
+		}
+		]
+	},
+	{
+		name:"console",
+		description:"prints out console.log() entries to a visible area on screen. useful for mobile debugging. output is scrollable. contains angular directive and service.",
+		versions:[
+		{
+			number:"1",
+			src:"console-1.js",
+			status:current
+		}
+		]
+	}
+	]
+
+	return {
+		modules:modules
+	}
+
+})
+
+.controller("controller", ['$scope', 'data', function ($scope, data) {
+
+	var self = this;
+
+	self.modules = data.modules;
+
+}])
+
+.directive("footer", function () {
+
+	return {
+		scope:false,
+		link:function ($scope, element, attr) {
+
+			var outer;
+			var footer;
+			var inner;
+
+			var setHeight = function () {
+
+				outer = $("#footerouter");
+				footer = $(element);
+				inner = $("#footerinner");
+
+				footer.css({height:$(window).height()});
+				//inner.css({bottom:"10%"});
+			}
+
+			setTimeout(function () {
+
+				setHeight();
+			}, 300);
+
+			$(window).on("resize", setHeight());
+		}
+	}
+
+})
+
+.directive("link", function () {
+
+	return {
+		scope:false,
+		link:function ($scope, element, attr) {
+
+			$(element).on("click", function () {
+
+				window.open("http://www.methodswithclass.com", "_blank");
+			})
+
+		}
+	}
+})
+
+.directive("download", function () {
+
+	return {
+
+		scope:false,
+		link:function ($scope, element, attr) {
+
+			$(element).on("click", function () {
+
+				window.open("http://code.methodswithclass.com/" + $scope.version.src, "_blank");
+			})
+		}
+	}
+
+})
--- index.php
+++ index.php
@@ -1,24 +1,134 @@
 <html>
 	<head>
 
-		<style>
+		<link rel="stylesheet" href="css/museo/museo300.css" type="text/css" charset="utf-8">
+	    <link rel="stylesheet" href="classes.css">
+	    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
-			.container {
-
-				position:absolute;
-				top:45%;
-				width:100%;
-				text-align:center;
-				font-size:50px;
-			}
-
-		</style>
+	    <script src="libs/jquery-1.11.3.min.js"></script>
+	    <script src="libs/jquery.scrollto.js"></script>
+	    <script src="libs/hammer.js"></script>
+	    <script src="libs/angular.min.js"></script>
+	    <script src="libs/angular-route.min.js"></script>
+	    <script src="libs/angular.ui-router.min.js"></script>
+	    <script src="libs/jquery.hammer.js"></script>
 
 	</head>
 
-	<body>
+	<body ng-app="app" ng-controller="controller as main">
 
-		<div class="container"> this page does not exist </div>
+		<div class="absolute fill scrollY touch" id="body">
+
+			<div class="relative width height-600 border-white cutoff" parallax scroll="body" inner="inner" top="true">
+
+				<div class="relative width height" id="inner">
+
+					<div class="relative width80 height80 center">
+
+						<div class="relative width height50">
+							<div class="absolute width text-right top0 font-70">
+								methods with<br>
+								class
+							</div>
+						</div>
+
+						<div class="relative width height50">
+							<div class="absolute width text-right bottom0 font-40">
+								code
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="relative width">
+
+				<div ng-repeat="module in main.modules">
+
+					<div class="relative width height-800 black-back">
+						<div class="absolute width40 height80 center white-back lowered">
+
+							<div class="absolute width80 height80 center">
+								<div class="relative width padding-v-20 font-40">
+									{{module.name}}
+								</div>
+
+								<div class="relative width padding-v-20 font-20">
+									{{module.description}}
+								</div>
+
+								<div class="relative width padding-v-20 font-20">
+
+									versions:
+								</div>
+
+								<div ng-repeat="version in module.versions">
+
+									<div class="relative width margin-v-20 height-50">
+
+										<div class="absolute width30 height font-20">
+											<div class="absolute center text-center">
+												{{version.number}}
+											</div>
+										</div>
+
+										<div class="absolute width30 height left30 font-20">
+											<div class="relative center text-center">
+												{{version.status}}
+											</div>
+										</div>
+
+										<div class="absolute width40 height left60 font-20">
+											<div class="absolute width height black-back white raised rounded10 pointer" download>
+												<div class="absolute center text-center ">
+													download
+												</div>
+											</div>
+										</div>
+
+									</div>
+
+								</div>
+
+							</div>
+
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="relative width height-400 cutoff" id="footerouter">
+
+				<div class="absolute width height" id="footer">
+
+					<div class="absolute width80 height-300 center" id="footerinner">
+						<div class="absolute width-200 height-50 border rounded20 text-right top20 right0 pointer" link>
+							<div class="absolute center">
+								visit the main page
+							</div>
+
+						</div>
+
+						<div class="absolute width text-right bottom0">
+							&copy;2016 Methods with Class
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+
+		<script src="app.js"></script>
+		<script src="shared-2.js"></script>
+		<script src="parallax-2.1.js"></script>
 
 	</body>
 </html>
