dfea9d58d680d14fde18142f6717b064cdb275e4
Christopher Polito <christopher.polito343@gmail.com>
2016-03-16 16:54:17 -0400

    update

--- api/shared-2.1.js
+++ api/shared-2.1.js
@@ -779,3 +779,15 @@ angular.module('sharedModule', [])
 	};
 })
 
+.directive('onHover', function () {
+	return function (scope, element, attrs) {
+		return $(element).hammer({
+			 	prevent_default: false,
+			 	drag_vertical: false
+			})
+			 .bind("hover", function (ev) {
+			   return scope.$apply(attrs['onHover']);
+			 });
+	};
+})
+
