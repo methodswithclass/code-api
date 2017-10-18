03bd9f4060fc7b8dd7410934694f706b25869572
Christopher Polito <chris@polito.io>
2017-10-17 20:13:14 -0400

    update

--- api/evolve/1.0/evolve.js
+++ api/evolve/1.0/evolve.js
@@ -53,6 +53,26 @@ application but are not related to the evolutionary algorithm, including them si
 	}
 
 
+	var shuffle = function (array) {
+		var currentIndex = array.length, temporaryValue, randomIndex;
+
+		// While there remain elements to shuffle...
+		while (0 !== currentIndex) {
+
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+
+	  	return array;
+	}
+
+
 	var individual = function (params) {
 			
 		var self = this;
