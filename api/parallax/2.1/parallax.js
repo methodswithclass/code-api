528c3d0cd06596ec333a4c6b4830f6585e6e6381
Christopher Polito <chris@mwithc.io>
2018-01-24 06:11:46 -0500

    update

--- api/parallax/2.1/parallax.js
+++ api/parallax/2.1/parallax.js
@@ -23,7 +23,7 @@ angular.module("parallaxModule", [])
 
 
 // determines whether current device can use parallax scrolling. IE and mobile devices return false.
-.factory("utility", function () {
+.factory("device", function () {
 
 	var valid = function() {
 
@@ -35,90 +35,8 @@ angular.module("parallaxModule", [])
 		return false;
 	}
 
-	var waitForElem = function (options, complete) {
-
-        var count = 0;
-        var result = false;
-        var active = {}
-
-        var checkElements = function (array) {
-
-        	result = false;
-        	active = {};
-
-        	if (Array.isArray(array)) {
-
-        		// console.log("###################\n\n\n\n\n\narray is array \n\n\n\n\n\n################")
-
-        		for (var i in array) {
-
-        			// console.log("element", array[i], "does not exist");
-
-	        		if ($(array[i])[0]) {
-	        			active[i] = true;
-	        		}
-
-        		}
-
-
-	        	if (Object.keys(active).length < array.length-1) {
-
-	        		result = true;
-	        	}
-	        	else {
-	        		result = false;
-	        	}
-
-        	}
-        	else {
-
-        		// console.log("@@@@@@@@@@@@@@@@\n\n\n\n\n\n\n\n\array is single\n\n\n\n\n\n@@@@@@@@@@@@@@")
-
-        		if ($(array)[0]) {
-        			// console.log("element does not exist");
-        			result = true;
-        		}
-        		else {
-        			result = false;
-        		}
-
-        	}
-
-        	return result;
-        }
-
-        var waitTimer = setInterval(function () {
-
-            if (checkElements(options.elems) || count >= 500) {
-
-            	// console.log("clear interval");
-
-                clearInterval(waitTimer);
-                waitTimer = null;
-
-                if (count < 500) {
-
-                	// console.log("run complete");
-                    
-                    if (typeof complete === "function") complete(options);
-                }
-                else {
-
-                	// console.log("count limit reached");
-                }
-                
-            }
-            else {
-
-                count++;
-            }
-
-        }, 30);
-    }
-
 	return {
-		valid:valid,
-		waitForElem:waitForElem
+		valid:valid
 	}
 
 
@@ -126,7 +44,7 @@ angular.module("parallaxModule", [])
 
 
 // add this directive to the element you want to add a parallax scrolling element too
-.directive('parallax', ['utility', '$window', function (u, $window) {
+.directive('parallax', ['device', '$window', function (device, $window) {
 
 
 	// link function, see below for parameters
@@ -134,52 +52,60 @@ angular.module("parallaxModule", [])
 
 
 		// adjusts the size of the image (defined in the directive 'src') to always be bigger than the parent
-		var fixInside = function (params) {
+		var fix = function (params) {
 
-			var i = params.inside;
-	    	var s = params.space;
-	    	
-	    	var iw = i.width;
-	    	var ih = i.height;
-	    	var sw = s.width;
-	    	var sh = s.height;
-
-	    	var ar = iw/ih;
+			var img = params.img;
+	    	var space = params.space;
+	    	var aspect;
 
 			var goodAspect = function (width, height) {
-				if (Math.abs(iw/ih - ar) < 0.01) return true;
+				if (Math.abs(width/height - aspect) < 0.01) return true;
 				return false;
 			}
 
-			var checkHeight = function ($h) {
-		        if ($h < sh) return "under";
-		        else if ($h > sh*1.2) return "over";
+			var checkHeight = function (height) {
+		        if (height < space.height()) return "under";
+		        else if (height > space.height()*1.2) return "over";
 		        return "good";
 		    }
 
-		    var checkWidth = function ($w) {
-		        if ($w < sw) return "under";
-		        else if ($w > sw*1.2) return "over";
+		    var checkWidth = function (width) {
+		        if (width < space.width()) return "under";
+		        else if (width > space.width()*1.5) return "over";
 		        return "good";
 		    }
 
-	        var h = space.height*1.2;
-	        var w = height*aspect;
+	    	if ($(img)[0]) {
+
+		    	if (params.first) aspect = img.width()/img.height();
+
+		        var height = space.height()*1.2;
+		        var width = height*aspect;
+		        
+		        if (checkWidth(width) == "under") {
+		            //console.log("width under " + name);
+		            width = space.width();
+		            height = width/aspect;
+		            if (checkHeight(height) == "under") {
+		                //console.log("height under " + name);
+		                height = space.height()*1.2;
+		                width = height*aspect;
+		            }
+		        }
+		        else if (checkWidth(width) == "over") {
+		            //console.log("width over " + name);
+		            width = space.width()*1.2;
+		            height = width/aspect;
+		            if (checkHeight(height) == "under") {
+		                //console.log("height under " + name);
+		                height = space.height()*1.2;
+		                width = height*aspect;
+		            }
+		        }
+
+		        img.css({height:height, width:width});
+	    	}
 	        
-	        if (checkWidth(w) != "good") {
-	            w = sw*1.2;
-	            h = w/ar;
-	            if (checkHeight(h) == "under") {
-	                h = sh*1.2;
-	                w = h*ar;
-	            }
-	        }
-
-	        return {
-	        	width:w,
-	        	height:h
-	        }
-
 	    }
 
 	    // generally solves a system of two linear equations 
@@ -283,24 +209,10 @@ angular.module("parallaxModule", [])
 
 		// get parallax scroll parameters, solve linear equation for current values, called when loaded and anytime the window is resized
 		var reset = function () {
-
-				
-			var ed = fixInside({
-				inside:{
-					width:$(img).width(), 
-					height:$(img).height()
-				}, 
-				space:{
-					width:$(element).width(),
-					height:$(element).height()
-				}
-			});
-
-			$(img).css({width:ed.width, height:ed.height});
-
-			ih = $(img).height() > 0 ? $(img).height() : ph*0.8;
-
-
+			if (img) {
+				fix({img:$(img), space:$(element), first:true});
+			}
+			ih = img ? $(img).height() : ph*0.8;
 			g = (ph-ih)/2;
 			h = $el.height();
 
@@ -325,12 +237,13 @@ angular.module("parallaxModule", [])
 
 			console.log("m:" + eqs.m + " b:" + eqs.b);
 			
+			
 		}
 
 		// changes height of parallax scrolling element based on element offset compared to top of scrolling element
 		var scroll = function () {
 			// if device is desktop and a parallax scrolling element is defined
-			if (u.valid() && active) {
+			if (device.valid() && active) {
 
 				o = $(element).offset().top - $el.offset().top;
 
@@ -345,44 +258,24 @@ angular.module("parallaxModule", [])
 		// initiate parallax elements when loaded,
 		// determine parallax values,
 		// and run scroll() once when loaded so that scrolling doesn't cause jump
-		// setTimeout(function () {
-			// setup();
-		// }, 200);
+		setTimeout(function () {
+			setup();
+		}, 200);
 
-		u.waitForElem({elems:[($scope.inner ? ("#" + $scope.inner) : $scope.src), element]}, function () {
+		setTimeout(function () {
 
-			setup()
-		});
-
-
-		u.waitForElem({elems:[img, $el, element, inner]}, function () {
-
-			
 			reset();
 			scroll();
-
-			angular.element($window).bind('resize', function () {
-				reset();
-				scroll();
-			});
-
-			$el.bind('scroll', scroll);
-		})
-
-		// setTimeout(function () {
-
-		// 	reset();
-		// 	scroll();
-		// }, 500);
+		}, 500);
 
 		// determine values and run the top setting function when the window is resized
-		// angular.element($window).bind('resize', function () {
-		// 	reset();
-		// 	scroll();
-		// });
+		angular.element($window).bind('resize', function () {
+			reset();
+			scroll();
+		});
 
 		// bind the top setting function for parallax to the scrolling event
-		// $el.bind('scroll', scroll);
+		$el.bind('scroll', scroll);
 
 	}
 
