2b01264775d65394c312b4a2a9dd92f1d5171e03
Christopher Polito <cpolito343@gmail.com>
2018-09-15 19:53:46 -0400

    update

--- config.js
+++ config.js
@@ -1,9 +1,89 @@
 
 
+var gulp = require("gulp");
+var merge = require("merge-stream");
+var imagemin = require('gulp-imagemin');
+
+var htmlDest = "dist/";
+
+var mainScripts = [
+   	"src/assets/js/supporting-code.js",
+    "api/parallax/5.0/parallax.js",
+    "api/shared/2.0/shared.js",
+    "src/state/stateModule.js",
+    "src/app.js",
+    "src/**/*.js"
+]
+
+
+var sassStyles = [
+	"api/classes/2.0/classes.scss",
+	"src/assets/css/styles.scss"
+]
+
+
+var cssStyles = [
+	'temp/**/*.css',
+	"node_modules/@fortawesome/fontawesome-free/css/all.css"
+]
+
+
+var shimFile = "node_modules/@babel/polyfill/dist/polyfill.js";
+
+
+var vendorScripts = [
+	"node_modules/jquery.scrollto/jquery.scrollTo.js"
+]
+
+
+var miscSrc = [
+	'src/assets/config/**/*.*'
+]
+
+
+// var minify = process.env.NODE_ENV == "production";
+
+var minify = {
+	main:{
+		full:{
+			make:true,
+			inject:false
+		},
+		min:{
+			make:false,
+			inject:true
+		}
+	},
+	vendor:{
+		full:{
+			make:true,
+			inject:true
+		},
+		min:{
+			make:false,
+			inject:false
+		}
+	}
+}
+
+
 
 var livereloadPort = 4120;
 
 
 module.exports = {
+	gulp:{
+		shimFile:shimFile,
+		htmlDest:htmlDest,
+		mainScripts:mainScripts,
+		vendorScripts:vendorScripts,
+		sassStyles:sassStyles,
+		cssStyles:cssStyles,
+		miscSrc:miscSrc,
+		minify:minify
+	},
 	livereloadPort:livereloadPort
 }
+
+
+
--- gulpfile_backup.js
+++ gulpfile_backup.js
@@ -14,27 +14,44 @@ mainBowerFiles = require("gulp-main-bower-files"),
 nodemon = require('gulp-nodemon'),
 livereload = require('gulp-livereload');
 sass = require("gulp-sass"),
-babel = require("gulp-babel");
+babel = require("gulp-babel"),
+
+babelPresets = require("./babel.config.js");
+
 
 const config = require("./config.js");
 
 
+// var minify = process.env.NODE_ENV == "production";
 
-var minify = config.gulp.minify;
-var shimFile = config.gulp.shimFile;
-var mainScripts = config.gulp.mainScripts;
-var vendorScripts = config.gulp.vendorScripts;
-var miscSrc = config.gulp.miscSrc;
-var htmlDest = config.gulp.htmlDest;
-var sassStyles = config.gulp.sassStyles;
-var cssStyles = config.gulp.cssStyles;
-
+var minify = {
+	main:{
+		full:{
+			make:true,
+			inject:true
+		},
+		min:{
+			make:false,
+			inject:true
+		}
+	},
+	vendor:{
+		full:{
+			make:true,
+			inject:true
+		},
+		min:{
+			make:false,
+			inject:false
+		}
+	}
+}
 
 
 var injectJS = function () {
 
-	var important = gulp.src('dist/assets/js/vendor' + (!minify.vendor.full.make && minify.vendor.min.make && minify.vendor.min.inject ? ".min" : "") + '.js', {read: false});
-	var standard = gulp.src(["dist/assets/js/main" + (!minify.main.full.make && minify.main.min.make && minify.main.min.inject ? ".min" : "") + ".js", 'dist/assets/**/*.css'], {read:false});
+	var important = gulp.src('dist/assets/js/vendor' + (minify.vendor.min.make && minify.vendor.min.inject ? ".min" : "") + '.js', {read: false});
+	var standard = gulp.src(["dist/assets/js/main" + (minify.main.min.make && minify.main.min.inject ? ".min" : "") + ".js", 'dist/assets/**/*.css'], {read:false});
 
 	return gulp.src('src/index.html')
 	.pipe(inject(important, {ignorePath:"dist", starttag: '<!-- inject:head:{{ext}} -->'}))
@@ -47,7 +64,14 @@ var injectJS = function () {
 var scripts = function() {
 
 
-    var mainSrc = gulp.src(mainScripts)
+    var mainSrc = gulp.src([
+                           	"src/assets/js/supporting-code.js",
+                            "api/parallax/5.0/parallax.js",
+                            "api/shared/2.0/shared.js",
+						    "src/state/stateModule.js",
+					        "src/app.js",
+					        "src/**/*.js"
+    ])
 	.pipe(concat('main.js'))
 	.pipe(babel({
         presets: ["@babel/env"]
@@ -73,6 +97,11 @@ var scripts = function() {
 
 var tempVendor = function () {
 
+	// var shimFile = "node_modules/angular-polyfills/dist/all.js";
+	// var shimFile = "node_modules/es6-shim/es6-shim.min.js";
+
+	var shimFile = "node_modules/@babel/polyfill/dist/polyfill.js";
+
 
 	var shim = gulp.src(shimFile)
 		.pipe(concat("shim.js"))
@@ -84,7 +113,11 @@ var tempVendor = function () {
 		.pipe(concat("bower.js"))
 		.pipe(gulp.dest("temp/vendor"));
 
-	var npmSrc = gulp.src(vendorScripts)
+
+	var npmSrc = gulp.src([
+			//npm packages for front end use
+			"node_modules/jquery.scrollto/jquery.scrollTo.js"
+			])
 		.pipe(concat("npm.js"))
 		.pipe(gulp.dest("temp/vendor"))
 
@@ -134,7 +167,10 @@ var vendor = function () {
 };
 
 var apiSass = function () {
-  return gulp.src(sassStyles)
+  return gulp.src([
+                  'src/assets/**/*.scss',
+                  "api/classes/2.0/classes.scss"
+                  ])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('temp/'));
 }
@@ -146,7 +182,10 @@ var styles = function() {
 	// middleware.compileSass();
 
 
-	var cssSrc = gulp.src(cssStyles)
+	var cssSrc = gulp.src([
+	                      'temp/**/*.css',
+	                      "node_modules/@fortawesome/fontawesome-free/css/all.css"
+	                      ])
 	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'));
 
 	
@@ -158,11 +197,10 @@ var styles = function() {
 };
 
 
-
 var html = function () {
 
 	return gulp.src('src/**/*.html')
-	.pipe(gulp.dest(htmlDest))
+	.pipe(gulp.dest("dist/"))
 };
 
 var images = function() {
@@ -170,7 +208,6 @@ var images = function() {
 	.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
 	.pipe(gulp.dest('dist/assets/img'));
 };
-
 
 var fonts = function () {
 
@@ -191,19 +228,12 @@ var index = function () {
 }
 
 var misc = function() {
-	// return gulp.src(miscSrc)
-	// .pipe(gulp.dest('dist/assets'));
-
-	// return merge(miscSrc);
-
-	if (typeof miscSrc === "function") {
-		return miscSrc();
-	}
-	else {
-		return gulp.src(miscSrc)
-		.pipe(gulp.dest('dist/assets'));
-	}
+	return gulp.src([
+	                'src/assets/config/**/*.*'
+	                ])
+	.pipe(gulp.dest('dist/assets/config'));
 };
+
 
 var clean = function() {
 	return del(['dist', "temp"]);
@@ -218,7 +248,7 @@ var serveFunc = function (done) {
 	var stream = nodemon({ 
 		script: path.join(__dirname, "server.js"),
 		ext:"js html css scss json",
-		watch:["./src", "config.js"],
+		watch:["./src", "./api", "./api-app"],
 		tasks:["build"]
 	});
 
